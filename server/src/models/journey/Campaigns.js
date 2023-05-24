9ffd237989200fc9218a2dd8fe1257d7ea7776dd
Kitti Born <born.kitti97@gmail.com>
2023-05-24 10:12:06 +0200

    refactor Campaign Scheam so that timestamp works and MyCharacter is not required anymore

--- server/src/models/journey/Campaigns.js
+++ server/src/models/journey/Campaigns.js
@@ -1,20 +1,20 @@
 import mongoose from "mongoose";
 
-const CampaignSchema = new mongoose.Schema({
-    timestamps: true,
+const CampaignSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        description: String,
 
-    title: { type: String, required: true },
-    description: String,
+        myCharacter: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "my_characters",
+        },
+        party: [{ type: mongoose.Schema.Types.ObjectId, ref: "party_members" }],
 
-    myCharacter: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "my_characters",
-        required: true,
+        sessions: [{ type: mongoose.Schema.Types.ObjectId, ref: "sessions" }],
+        // notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "notes" }],
     },
-    party: [{ type: mongoose.Schema.Types.ObjectId, ref: "party_members" }],
-
-    sessions: [{ type: mongoose.Schema.Types.ObjectId, ref: "sessions" }],
-    // notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "notes" }],
-});
+    { timestamps: true }
+);
 
 export const CampaignModel = mongoose.model("campaigns", CampaignSchema);
