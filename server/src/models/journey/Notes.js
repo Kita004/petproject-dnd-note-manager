186b73f6106021e57348a1aec79f999b9088e75d
Kitti Born <born.kitti97@gmail.com>
2023-06-05 11:18:14 +0200

    refactor Schemas so that timestamp works

--- server/src/models/journey/Notes.js
+++ server/src/models/journey/Notes.js
@@ -1,18 +1,23 @@
 import mongoose from "mongoose";
 
-const NoteSchema = new mongoose.Schema({
-    timestamps: { createdAt: true, updatedAt: false },
-    type: {
-        type: String,
-        enum: ["npc", "item", "place", "misc"],
-        default: "misc",
+const NoteSchema = new mongoose.Schema(
+    {
+        type: {
+            type: String,
+            enum: ["npc", "item", "place", "misc"],
+            default: "misc",
+        },
+
+        title: { type: String, required: true },
+        comments: [String],
+
+        sessionOwner: { type: mongoose.Schema.Types.ObjectId, ref: "sessions" },
+        campaignOwner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "campaigns",
+        },
     },
-
-    title: { type: String, required: true },
-    comments: [String],
-
-    sessionOwner: { type: mongoose.Schema.Types.ObjectId, ref: "sessions" },
-    campaignOwner: { type: mongoose.Schema.Types.ObjectId, ref: "campaigns" },
-});
+    { timestamps: { updatedAt: false } }
+);
 
 export const NoteModel = mongoose.model("notes", NoteSchema);
--- server/src/models/journey/Sessions.js
+++ server/src/models/journey/Sessions.js
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
 
-const SessionSchema = new mongoose.Schema({
-    timestamps: { createdAt: true, updatedAt: false },
+const SessionSchema = new mongoose.Schema(
+    {
+        title: String,
+        events: [String],
+        notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "notes" }],
 
-    title: String,
-    events: [String],
-    notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "notes" }],
-
-    campaignOwner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "campaigns",
-        required: true,
+        campaignOwner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "campaigns",
+            required: true,
+        },
     },
-});
+    { timestamps: { updatedAt: false } }
+);
 
 export const SessionModel = mongoose.model("sessions", SessionSchema);
