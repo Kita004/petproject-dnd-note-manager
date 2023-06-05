cdcf215a7f20fdfcbe4aee070e41f1f02405aff2
Kitti Born <born.kitti97@gmail.com>
2023-06-05 11:00:51 +0200

    add route to index

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,11 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 import { userRouter } from "./routes/users.js";
 import { campaignRouter } from "./routes/campaigns.js";
+import { sessionRouter } from "./routes/sessions.js";
+import { notesRouter } from "./routes/notes.js";
 
 dotenv.config();
 
@@ -14,7 +17,9 @@ app.use(cors());
 
 mongoose.connect(process.env.ATLAS_URI);
 
-app.use("/api/auth", userRouter);
+app.use("/api/users", userRouter);
 app.use("/api/campaigns", campaignRouter);
+app.use("/api/sessions", sessionRouter);
+app.use("/api/notes", notesRouter);
 
 app.listen(3001, () => console.info("SERVER STARTED!"));
